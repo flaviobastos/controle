ab9d4bfba54ffc103ec8d02a8dfa471b570f47f2
“flaviobastos” <flavio.bastos@hotmail.com>
2024-08-20 12:32:28 -0300

    Create Login component and Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Livewire\Login;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', Login::class)->name('login');
 });
+
+/* Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', index::class)->name('dashboard');
+}); */
